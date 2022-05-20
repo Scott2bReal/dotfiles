4b599827056bce9c1089b93beeed35f15a8f16f5
Scott Hoecker <hoeckers@gmail.com>
2022-05-20 00:25:42 -0500

    Change PATH order

--- fish/.config/fish/options.fish
+++ fish/.config/fish/options.fish
@@ -7,10 +7,10 @@ set -gx VISUAL /usr/bin/nvim
 set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
 
 # Construct PATH
-fish_add_path /usr/local/sbin
-fish_add_path /usr/local/bin
-fish_add_path /usr/bin
 fish_add_path /bin
+fish_add_path /usr/bin
+fish_add_path /usr/local/bin
+fish_add_path /usr/local/sbin
 fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin # Rust
 fish_add_path ~/.rbenv/bin # Ruby (thru rbenv)
