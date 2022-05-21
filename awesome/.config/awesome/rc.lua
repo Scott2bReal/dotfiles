bcea4fea3dd841720ad21deba33309d7f4392f7b
Scott Hoecker <hoeckers@gmail.com>
2022-05-21 09:54:57 -0500

    Poke around awesome configs

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -59,16 +59,16 @@ end
 beautiful.init("~/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "alacritty"
-editor = os.getenv("EDITOR") or "nano"
-editor_cmd = terminal .. " -e " .. editor
+local terminal = "alacritty"
+local editor = os.getenv("EDITOR") or "nano"
+local editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
 -- Usually, Mod4 is the key with a logo between Control and Alt.
 -- If you do not like this or do not have such a key,
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
+local modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
@@ -279,7 +279,6 @@ root.buttons(gears.table.join(
 ))
 -- }}}
 
-require "keybinds"
 -- {{{ Key bindings
 globalkeys = gears.table.join(
   awful.key({ modkey }, "s", hotkeys_popup.show_help, { description = "show help", group = "awesome" }),
